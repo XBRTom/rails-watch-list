bd0e72bf12b9cd50f42d12a65eac9dfbf74c3cc0
Brice Roehrig <broehrig.contact@gmail.com>
2022-03-17 11:36:20 +0100

    mig ok + validation set

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks
-  has_many :movies, dependent: :destroy
+  has_many :movies, :throught => :bookmarks, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 end
